b29f213c2c963d3952aa346815726f7359e1efff
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-07-07 23:28:10 -0300

    Liftoff

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from './components/Button'
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+export default App;
